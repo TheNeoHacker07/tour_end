b323dfa79b3f5eaa28d487d82fe553b8ac9a4f2a
TheNeoHacker07 <tsstark225@gmail.com>
2024-10-24 20:32:34 +0600

    asd

--- tour/filters.py
+++ tour/filters.py
@@ -7,11 +7,11 @@ class TourFilter(django_filters.FilterSet):
     price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
     start_date = django_filters.DateFilter(field_name='start_date', lookup_expr='gte')
     end_date = django_filters.DateFilter(field_name='end_date', lookup_expr='lte')
-    place = django_filters.CharFilter(field_name='place', lookup_expr='icontains')
+    # place = django_filters.CharFilter(field_name='place', lookup_expr='icontains')
 
     class Meta:
         model = Tour
-        fields = ['price', 'start_date', 'end_date', 'place']
+        fields = ['price', 'start_date', 'end_date']
 
     
 
