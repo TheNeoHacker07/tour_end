0340f972efb331d09faaa000b642167bab2ddf8a
TheNeoHacker07 <tsstark225@gmail.com>
2024-11-01 21:58:07 +0600

    sdf

--- tour/admin.py
+++ tour/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Booking, Car, Review, Tour, TourGroupDetail, TourThemes, TourType, Image, ImageCar
+from .models import Booking, Car, Review, Tour, TourGroupDetail, TourThemes, TourType, Image, ImageCar, Region, Country
 
 admin.site.register(Booking)
 admin.site.register(Car)
@@ -10,3 +10,5 @@ admin.site.register(TourThemes)
 admin.site.register(TourType)
 admin.site.register(Image)
 admin.site.register(ImageCar)
+admin.site.register(Region)
+admin.site.register(Country)
